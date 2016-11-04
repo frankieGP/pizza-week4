43a28f451695100bc93f3f82870df8b9820dd142
Frankie Gavin <frankanthonygavin@gmail.com>
2016-11-04 13:28:20 -0700

    change radio form for toppings to a drop down selector, comment out back-end logic, only size or topping will display, but not both.

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,49 @@
-//back-end
-var toppingE = ["Artichoke", "Feta" ]
-var toppingC = ["Pineapple", "Olives"]
-var pizzaSize = ["Small", "Medium", "Large", "XL"]
-
-function Pizza(pizzaSize, topping) {
-  this.pizzaSize = pizzaSize;
-  this.topping = topping;
-  this.price = 10;
-}
-
-  Pizza.prototype.pizzaPrice = function() {
-    if(toppingE.includes(this.topping) === true){
-      this.price +=2;
-    }
-    if(toppingC.includes(this.topping) === true){
-      this.price +=1;
-    }
-    if(this.pizzaSize === "Small"){
-      this.price +=1;
-    }
-    if(this.pizzaSize === "Medium"){
-      this.price +=2;
-    }
-    if(this.pizzaSize === "Large"){
-      this.price +=3;
-    }
-    if(this.pizzaSize === "XL"){
-      this.price +=4;
-    }
-  }
+// //back-end
+// var toppingE = ["Artichoke", "Feta" ]
+// var toppingC = ["Pineapple", "Olives"]
+// var pizzaSize = ["Small", "Medium", "Large", "XL"]
+//
+// function Pizza(pizzaSize, topping) {
+//   this.pizzaSize = pizzaSize;
+//   this.topping = topping;
+//   this.price = 10;
+// }
+//
+//   Pizza.prototype.pizzaPrice = function() {
+//     if(toppingE.includes(this.topping) === true){
+//       this.price +=2;
+//     }
+//     if(toppingC.includes(this.topping) === true){
+//       this.price +=1;
+//     }
+//     if(this.pizzaSize === "Small"){
+//       this.price +=1;
+//     }
+//     if(this.pizzaSize === "Medium"){
+//       this.price +=2;
+//     }
+//     if(this.pizzaSize === "Large"){
+//       this.price +=3;
+//     }
+//     if(this.pizzaSize === "XL"){
+//       this.price +=4;
+//     }
+//   }
 //front-end
 $(document).ready(function() {
  $("#pizza-builder").submit(function(event) {
    event.preventDefault();
 
-    var size = $("#pizza-size").val()
-    var topping = $("input:radio[name=topping]:checked").val();
-    var userPizza = new Pizza(pizzaSize, topping);
+    var inputSize = $("#pizza-size").val()
+    var inputTopping = $("#topping").val();
+    //var userPizza = new Pizza(pizzaSize, topping);
 
-    Pizza.pizzaPrice();
+    //Pizza.pizzaPrice();
 
     //$(".results").show();
-    $("#size-result").text(userPizza.pizzaSize);
-    $("#topping-result").text(userPizza.topping);
-    $("#pizza-price").text(userPizza.price);
+    $("#size-result").text(inputSize);
+    $("#topping-result").text(inputTopping);
+    //$("#pizza-price").text(userPizza.price);
       //console.log(userTicket);
       //console.log("After processing: $" + userTicket.price);
 
