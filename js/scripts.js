1ecc11189b452f78eb3974e71188e158d8f46d11
Frankie Gavin <frankanthonygavin@gmail.com>
2016-11-04 13:39:38 -0700

    add back-end logic back, but price is not showing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,34 @@
 // //back-end
-// var toppingE = ["Artichoke", "Feta" ]
-// var toppingC = ["Pineapple", "Olives"]
-// var pizzaSize = ["Small", "Medium", "Large", "XL"]
-//
-// function Pizza(pizzaSize, topping) {
-//   this.pizzaSize = pizzaSize;
-//   this.topping = topping;
-//   this.price = 10;
-// }
-//
-//   Pizza.prototype.pizzaPrice = function() {
-//     if(toppingE.includes(this.topping) === true){
-//       this.price +=2;
-//     }
-//     if(toppingC.includes(this.topping) === true){
-//       this.price +=1;
-//     }
-//     if(this.pizzaSize === "Small"){
-//       this.price +=1;
-//     }
-//     if(this.pizzaSize === "Medium"){
-//       this.price +=2;
-//     }
-//     if(this.pizzaSize === "Large"){
-//       this.price +=3;
-//     }
-//     if(this.pizzaSize === "XL"){
-//       this.price +=4;
-//     }
-//   }
+var toppingE = ["Artichoke", "Feta" ]
+var toppingC = ["Pineapple", "Olives"]
+var pizzaSize = ["Small", "Medium", "Large", "XL"]
+
+function Pizza(pizzaSize, topping) {
+  this.pizzaSize = pizzaSize;
+  this.topping = topping;
+  this.price = 10;
+}
+
+  Pizza.prototype.pizzaPrice = function() {
+    if(toppingE.includes(this.topping) === true){
+      this.price +=2;
+    }
+    if(toppingC.includes(this.topping) === true){
+      this.price +=1;
+    }
+    if(this.pizzaSize === "Small"){
+      this.price +=1;
+    }
+    if(this.pizzaSize === "Medium"){
+      this.price +=2;
+    }
+    if(this.pizzaSize === "Large"){
+      this.price +=3;
+    }
+    if(this.pizzaSize === "XL"){
+      this.price +=4;
+    }
+  }
 //front-end
 $(document).ready(function() {
  $("#pizza-builder").submit(function(event) {
@@ -36,14 +36,14 @@ $(document).ready(function() {
 
     var inputSize = $("#pizza-size").val()
     var inputTopping = $("#topping").val();
-    //var userPizza = new Pizza(pizzaSize, topping);
+    var userPizza = new Pizza(pizzaSize, topping);
 
-    //Pizza.pizzaPrice();
+    userPizza.pizzaPrice();
 
     //$(".results").show();
     $("#size-result").text(inputSize);
     $("#topping-result").text(inputTopping);
-    //$("#pizza-price").text(userPizza.price);
+    $("#pizza-price").text(userPizza.price);
       //console.log(userTicket);
       //console.log("After processing: $" + userTicket.price);
 
