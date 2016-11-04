3367390f1bc55694900e40c1a00dad5392e78af0
Frankie Gavin <frankanthonygavin@gmail.com>
2016-11-04 10:56:01 -0700

    add pizza constructor with properties for topping, size, and price and price prototype with if statments to adjust the price of the pizza. radio selector for toppings not yet functional.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,46 @@
+//back-end
+var toppingE = ["Artichoke", "Feta" ]
+var toppingC = ["Pineapple", "Olives"]
+var pizzaSize = ["Small", "Medium", "Large", "XL"]
+function Pizza(pizzaSize, topping) {
+  this.pizzaSize = pizzaSize;
+  this.topping = topping;
+  this.price = 10;
+}
 
+  Pizza.prototype.pizzaPrice = function() {
+    if(toppingE.includes(this.topping) === true){
+      this.price +=2;
+    }
+    if(toppingC.includes(this.topping) === true){
+      this.price +=1;
+    }
+    if(this.pizzaSize === "Small"){
+      this.price +=1;
+    }
+    if(this.pizzaSize === "Medium"){
+      this.price +=2;
+    }
+    if(this.pizzaSize === "Large"){
+      this.price +=3;
+    }
+    if(this.pizzaSize === "XL"){
+      this.price +=4;
+    }
+  }
 //front-end
 $(document).ready(function() {
-  var pizza =
- $("#size").submit(function(event) {
+ //$("#size").submit(function(event) {
+   event.preventDefault();
+   var size = $("#pizza-size").val()
+
     $("#topping").submit(function(event) {
 
-    event.preventDefault();
-    var size = $("#pizza-size").val()
-    var topping = $("input:radio[name=topping]:checked").val();
 
+    var topping = $("input:radio[name=topping]:checked").val();
+alert(topping);
     $("#size-result").text(size);
     $("#topping-result").text(topping);
-
 
   });
 });
